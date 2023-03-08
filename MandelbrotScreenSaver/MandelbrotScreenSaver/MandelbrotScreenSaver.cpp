d852b986c620c87aa6dafc6773a816cb8303eea9
JmDeleon2000 <dele19817@uvg.edu.gt>
2023-03-08 16:42:59 -0600

    Proyecto con CMAKE

---
+++ MandelbrotScreenSaver/MandelbrotScreenSaver/MandelbrotScreenSaver.cpp
@@ -0,0 +1,38 @@
+﻿// MandelbrotScreenSaver.cpp : Defines the entry point for the application.
+//
+#include <SDL.h>
+#include "MandelbrotScreenSaver.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("Mandelbrot set screen saver", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 512, 512, 0);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	bool running = true;
+	while (running)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+			case SDL_QUIT:
+				running = false;
+				break;
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderDrawPoint(renderer, 100, 100);
+
+		SDL_RenderPresent(renderer);
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+## ¡Importante para Windows!
+El proyecto requiere que la variable de ambiente CMAKE_PREFIX_PATH contenga el directorio a una instalación de un SDK de <a href="https://github.com/libsdl-org/SDL/releases/download/release-2.26.4/SDL2-devel-2.26.4-VC.zip">SDL</a>
