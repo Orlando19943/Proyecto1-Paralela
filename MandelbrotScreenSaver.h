8bde9cbe192d3a35361d40616ef9595534929e12
DiaZa13 <cor191025@uvg.edu.gt>
2023-03-19 22:38:11 -0600

    changing the compile configurations

---
+++ MandelbrotScreenSaver.h
@@ -0,0 +1,23 @@
+﻿// MandelbrotScreenSaver.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+
+struct complex 
+{
+	double real	= 0;
+	double img	= 0;
+};
+
+constexpr inline complex complexSquare(const complex & a) noexcept;
+
+inline double complex_sqr_mag(const complex& in) noexcept;
+
+void drawMandelbrot(int w, int h, double x_min, double y_min, double x_max, double y_max, SDL_Surface** target);
+
+constexpr inline complex operator+(const complex& a, const complex& b);
